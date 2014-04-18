a9b00b4674f941d18e64bd60c1b3108afa4bb6c4
kdhageman <kasjuhh@gmail.com>
2014-04-18 18:35:12 +0200

    Added shadows to the ships.

---
+++ Assets/scripts/Shadow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shadow : MonoBehaviour {
+
+	public float y_offset=-0.1f;
+	public float x_offset;
+
+	// Use this for initialization
+	void Start () {
+		x_offset = transform.localPosition.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = new Vector2(transform.parent.position.x + x_offset, transform.parent.position.y + y_offset);
+	}
+}
