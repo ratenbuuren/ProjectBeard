6ecafca5f1073b23e9c55b6f01a9ee381bdbbdbf
ratenbuuren <ratenbuuren@gmail.com>
2014-04-21 15:19:44 +0200

    No Beard and some AI stuff
    No Beard and some AI stuff

---
+++ Assets/scripts/player/Captain_NoBeard.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Captain_NoBeard : Captain{
+	
+	public string name;
+	public int id;
+	
+	public Captain_NoBeard() : base("No Beard" , 0){		
+	}
+	
+	public override void Activate(){
+	}
+	
+	public override void Deactivate(){
+	}
+}
