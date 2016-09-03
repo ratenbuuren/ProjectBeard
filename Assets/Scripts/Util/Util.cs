b0c1afaaac1922300c05da4988cf075e67886c24
kdhageman <kasparhageman@gmail.com>
2016-09-03 14:09:34 +0200

    Introduce Util class for mathematics

---
+++ Assets/Scripts/Util/Util.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+public static class Util {
+
+    private static double degreeToRadian(double angle)
+    {
+        return Math.PI * angle / 180.0;
+    }
+
+    public static double radiansToDegrees(double angle)
+    {
+        return angle * (180.0 / Math.PI);
+    }
+
+    public static double angle(Vector2 from, Vector2 to)
+    {
+        return radiansToDegrees(Math.Acos(Vector2.Dot(from.normalized, to.normalized)));
+    }
+}
