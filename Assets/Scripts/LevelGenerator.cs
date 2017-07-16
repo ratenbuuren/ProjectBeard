db783b38809c8169b8a04322df7d1849d6d1e890
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-07-16 13:09:54 +0200

    Add level edge

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,43 +10,97 @@ public class LevelGenerator : MonoBehaviour {
 	public int height = 10;
 	public int width = 10;
 	public GameObject tile = null;
+	public GameObject edge = null;
+
+	private GameObject root = null;
 
 	void Start () {
 		if (tile == null) {
 			throw new Exception ("Cannot create level out of empty tiles");
 		}
 
-		GameObject root = new GameObject (rootName);
+		root = new GameObject (rootName);
 
+		generatePlayingField ();
+		generateEdges ();
+	}
+
+	private void generatePlayingField() {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 
 				float xPos = (float) x - (width / 2f) + 0.5f;
 				float yPos = (float) y - (height / 2f) + 0.5f;
 
-				tile = new PrefabBuilder (tile)
+				new PrefabBuilder (tile)
 					.position (xPos, yPos)
 					.parent (root)
 					.build ();
 			}
 		}
 	}
+
+	private void generateEdges() {
+		// create top and bottom row
+		for (int x = 0; x < width+2; x++) { 
+			float xPos = (float)x - (width / 2f) - 0.5f;
+			float yPos = (height / 2f) + 0.5f;
+
+			new PrefabBuilder (edge)
+				.position (xPos, yPos)
+				.parent (root)
+				.build ();
+
+			yPos = -yPos;
+
+			new PrefabBuilder (edge)
+				.position (xPos, yPos)
+				.parent (root)
+				.build ();
+		}
+
+		// create left and right column
+		for (int y = 0; y < height; y++) {
+			float xPos = (width / 2) + 0.5f;
+			float yPos = (float) y - (height / 2f) + 0.5f;
+
+			new PrefabBuilder (edge)
+				.position (xPos, yPos)
+				.parent (root)
+				.build ();
+
+			xPos = -xPos;
+
+			new PrefabBuilder (edge)
+				.position (xPos, yPos)
+				.parent (root)
+				.build ();
+		}
+	}
 		
 	private class PrefabBuilder {
 
 		private GameObject obj;
+		private GameObject template;
 
-		public PrefabBuilder(GameObject obj) {
-			this.obj = Instantiate(obj);
+		public PrefabBuilder(GameObject template) {
+			this.template = template;
+			this.obj = Instantiate(template);
 		}
 
 		public PrefabBuilder position(float x, float y) {
 			obj.transform.position = new Vector2 (x, y);
+			obj.name = String.Format ("{0} [{1},{2}]", template.name, x, y);
 			return this;
 		}
 
 		public PrefabBuilder parent(GameObject parent) { 
 			obj.transform.parent = parent.transform;
+			return this;
+		}
+
+		public PrefabBuilder rotate(float angle) {
+			obj.transform.Rotate(new Vector3(0, 0, angle));
 			return this;
 		}
 
