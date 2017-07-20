fc89278764b043902369b47c5522f197842ef221
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-07-20 11:18:32 +0200

    Remove unused imports

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System;
-using System.Security.Cryptography.X509Certificates;
 
 public class LevelGenerator : MonoBehaviour {
 
