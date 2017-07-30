d49a7fa817bb9b0dd290d1236173e80104c80af2
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-07-30 20:33:49 +0200

    Let AI deal with no players

--- Assets/Scripts/Tanks/AITank.cs
+++ Assets/Scripts/Tanks/AITank.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AITank : BaseShooting {
     public float fireRangeThreshold = 4f;
@@ -6,11 +7,19 @@ public class AITank : BaseShooting {
 
     protected override void Start() {
         base.Start();
-        player = GameObject.FindGameObjectWithTag("Player");
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        if (allPlayers.Length > 0) {
+            player = GameObject.FindGameObjectsWithTag("Player")[0];
+        } else {
+            Debug.Log("Failed to find any player");
+        }
         barrelTransform = transform.Find("Barrel");
     }
 
     void Update() {
+        if (player == null) {
+            return;
+        }
         Vector3 playerPosition = player.transform.position;
         if (Vector3.Distance(transform.position, playerPosition) > fireRangeThreshold) {
             float step = stats.GetStat(StatType.MovementSpeed) * Time.deltaTime;
