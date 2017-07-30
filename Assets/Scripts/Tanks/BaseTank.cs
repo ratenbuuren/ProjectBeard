0cd9bbeeee37d4adf21e860d7ad26d0dbf29e7f3
Robin ten Buuren <robin.tenbuuren@nedap.com>
2017-07-30 14:16:53 +0200

    3 player mode and cleanup

---
+++ Assets/Scripts/Tanks/BaseTank.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTank : MonoBehaviour
+{
+	public float health = 100f;
+
+	public float GetHealth()
+	{
+		return health;
+	}
+
+	public void TakeDamage(float amount)
+	{
+		health -= amount;
+		if (health <= 0)
+		{
+			GameManager.instance.RemovePlayer(this.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
